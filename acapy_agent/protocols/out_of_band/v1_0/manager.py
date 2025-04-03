6424c97708ea0a260727668e3d224639c47c06e1
Patrick St-Louis <43082425+PatStLouis@users.noreply.github.com>
2025-04-03 14:42:21 -0400

    Fetch existing invitation route (#3572)
    * fetch existing invitation route
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * lint
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * fetch by oob_id
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * linting
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * change to path parameter
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * return entire record
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * test new route and manager function
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * lint
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * fix tests
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * return class from function
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * add fn return type
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    * add query parameter for oob_id and fix unit test
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>
    
    ---------
    
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- acapy_agent/protocols/out_of_band/v1_0/manager.py
+++ acapy_agent/protocols/out_of_band/v1_0/manager.py
@@ -1207,6 +1207,19 @@ class OutOfBandManager(BaseConnectionManager):
             for conn_rec in conn_records:
                 await conn_rec.delete_record(session)
 
+    async def fetch_oob_invitation_record_by_id(self, oob_id: str) -> OobRecord:
+        """Fetch oob_record associated with an oob_id."""
+        async with self.profile.session() as session:
+            oob_record = await OobRecord.retrieve_by_id(
+                session,
+                record_id=oob_id,
+            )
+
+        if not oob_record:
+            raise StorageNotFoundError(f"No record found with oob_id {oob_id}")
+
+        return oob_record
+
     async def delete_conn_and_oob_record_invitation(self, invi_msg_id: str):
         """Delete conn_record and oob_record associated with an invi_msg_id."""
         async with self.profile.session() as session:
@@ -1215,7 +1228,6 @@ class OutOfBandManager(BaseConnectionManager):
                 tag_filter={
                     "invitation_msg_id": invi_msg_id,
                 },
-                post_filter_positive={},
             )
             for conn_rec in conn_records:
                 await conn_rec.delete_record(session)
@@ -1224,7 +1236,6 @@ class OutOfBandManager(BaseConnectionManager):
                 tag_filter={
                     "invi_msg_id": invi_msg_id,
                 },
-                post_filter_positive={},
             )
             for oob_rec in oob_records:
                 await oob_rec.delete_record(session)
