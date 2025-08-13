37d342c8956f1cfe88f264da9554fe327915b52e
ff137 <ff137@proton.me>
2025-08-13 15:49:18 +0200

    :bug:

--- acapy_agent/anoncreds/revocation.py
+++ acapy_agent/anoncreds/revocation.py
@@ -1721,6 +1721,7 @@ class AnonCredsRevocation:
             options (dict): handling options
 
         """
+        options = options or {}
         LOGGER.info(
             "Emitting full registry event for cred def id: %s, rev reg def id: %s. "
             "request_id: %s, correlation_id: %s",
@@ -1729,7 +1730,6 @@ class AnonCredsRevocation:
             options.get("request_id"),
             options.get("correlation_id"),
         )
-        options = options or {}
 
         # Emit event to indicate full registry detected
         event = RevRegFullDetectedEvent.with_payload(
